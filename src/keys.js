4a38bd1362733d62ffd69b87145d9ab6110c820d
FelipeHernandez98 <andresfelipehc@ufps.edu.co>
2021-12-14 23:20:14 -0500

    Proyecto pc-ami completo

---
+++ src/keys.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+   database: {
+/*
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'pcamidb'*/
+
+          host: 'br4tza5ckqugbvcbwevu-mysql.services.clever-cloud.com',
+          user: 'u011hesusulc4j2g',
+          password: 'MChAiALE0K6TN0PthSoz',
+          database: 'br4tza5ckqugbvcbwevu'
+   }
+}
